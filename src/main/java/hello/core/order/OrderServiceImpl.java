b3bbe7ac2acc51f8d3c8beb0232582891def4d0c
jeongcode <58499373+jeongcode@users.noreply.github.com>
2020-12-28 23:55:29 +0900

    의존관계 자동 주입 - 다양한 의존관계 주입 방법

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -18,6 +18,8 @@ public class OrderServiceImpl implements OrderService{
         this.discountPolicy = discountPolicy;
     }
 
+
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
